c973da5adc3a89a25f0dd2915ee5eb780346272b
Lucas Mol Leite <lucas_mol@hotmail.com>
2021-06-06 09:22:36 -0300

    Mudança de nome de variáveis e comentários no código

--- api.js
+++ api.js
@@ -1,12 +1,14 @@
 module.exports = {
   handleRequest: function (request, response) {
     var https = require("https");
-    var cheerio = require("cheerio");
-    var dataURL = [];
     var baseURL = "https://" + request.headers.host + "/";
-    let path = new URL(request.url, baseURL);
+    var path = new URL(request.url, baseURL);
+    //Switch para as rotas da API
     switch (path.pathname) {
       case "/scrap_product":
+        let cheerio = require("cheerio");
+        let dataURL = [];
+        //Carrega os dados do site passado pela API
         https
           .get(path.searchParams.get("url"), (res) => {
             res.on("data", (data) => {
@@ -15,31 +17,31 @@ module.exports = {
             res.on("end", () => {
               dataURL = Buffer.concat(dataURL).toString();
               let $ = cheerio.load(dataURL);
-              var availability = true;
-
+              var availability_product = true;
+              //Verifica se existe tag de produto indisponível e preenche os campos de acordo
               if ($(".unavailable__product").length) {
                 var title = $("h1").text();
                 var imagelink = $(".unavailable__product-img").attr("src");
                 var priceJSON = JSON.parse(
                   $(".icon-fav-outline").attr("data-product")
                 );
-                var price = priceJSON.price;
+                var price_product = priceJSON.price;
                 var url = path.searchParams.get("url");
-                availability = false;
+                availability_product = false;
               } else {
                 var title = $("h1").text();
                 var imagelink = $(".showcase-product__big-img").attr("src");
-                var price = parseFloat(
+                var price_product = parseFloat(
                   $(".price-template__text").text().replace(",", ".")
                 );
                 var url = path.searchParams.get("url");
               }
-
+              //Monta o JSON para retorno da requisição
               let productJson = {
                 name: title,
                 image_primary: imagelink,
-                price: price,
-                availability: availability,
+                price: price_product,
+                availability: availability_product,
                 url: url,
               };
               let product = JSON.stringify(productJson);
@@ -52,17 +54,18 @@ module.exports = {
           })
           .on("error", (error) => {
             response.writeHead(404);
-            response.write("Houve erro na requisição");
+            response.write("Houve erro na requisição. " + error);
             response.end();
-            console.error(error);
           });
         break;
       case "/status":
+        //Retorna Ok no body como resposta a requisição
         response.writeHead(200, { "Content-Type": "text/html" });
         response.write("OK");
         response.end();
         break;
       default:
+        //Caso nenhuma rota definida seja feita na requisição
         response.writeHead(404);
         response.write("Rota não definida");
         response.end();
