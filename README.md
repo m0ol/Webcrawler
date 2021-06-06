9c654fdc2afe9f5a73133bf30e105690a22fea7e
Lucas Mol Leite <lucas_mol@hotmail.com>
2021-06-05 21:57:09 -0300

    Primeira versão do Readme e variáveis rescritas para melhor entendimento do código

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Webcrawler
+
+Requisitos:
+
+1-Instale o node acessando o site: https://nodejs.org/en/download/
+2-Instale um programa para fazer as requisições ex: Postman, Curl
+
+Como testar o código:
+
+1-Baixe o código do repositório
+2-Abre o prompt de comando e acesse a raiz do projeto do código do repositório
+3-Digite no prompt o comando "node server.js" para ligar o servidor local para fazer as requisições
+4-Utilizando algum dos programas anteriormente baixados, realize a requisição Get utilizando o link: http://localhost:8080/status Em caso de sucesso irá retornar "Ok" no body.
+5- Copie o link de algum produto no site do www.magazineluiza.com.br
+6-Realize a requisição post conforme o exemplo a seguir: http://localhost:8080/scrap_product?url=https://www.magazineluiza.com.br/porta-pet-door-para-caes-e-gatos-ate-7kg-com-trava-4-funcoes-desembrulha-presentes/p/ecd90def61/pe/pstg/
+7- Se for um link válido ele devera retornar as informações: nome do produto, link da imagem do produto, preço, disponibilidade do produto e URL do produto.
--- api.js
+++ api.js
@@ -1,65 +1,71 @@
-const { match } = require('assert');
-
 module.exports = {
-    handleRequest: function(request, response){
-        var https = require('https');
-        var cheerio = require('cheerio');
-        var dataURL = [];
-        var baseURL = 'https://' + request.headers.host + '/';
-        let path = new URL(request.url,baseURL);
-        switch(path.pathname){
-            case '/scrap_product':
-                https.get(path.searchParams.get('url'), (res) => {
-                    res.on('data', (d) => {
-                        dataURL.push(d);
-                    });
-                    res.on('end', () => {
-                        dataURL = Buffer.concat(dataURL).toString();
-                       let $  = cheerio.load(dataURL,{xmlMode: true});
-                       var availability = true
-                       if($('.unavailable__product').length){
-                        var title = $('h1').text();
-                        var imagelink = $('.unavailable__product-img').attr('src');
-                        var priceJSON = JSON.parse($('.icon-fav-outline').attr('data-product'));
-                        var price = priceJSON.price;
-                        var url = path.searchParams.get('url');
-                        availability = false;
-                        }else{
-                            var title = $('h1').text();
-                            var imagelink = $('.showcase-product__big-img').attr('src');
-                            var price = parseFloat($('.price-template__text').text().replace(",","."));
-                            var url = path.searchParams.get('url');
-                        }            
-                let payload = {
-                    name: title,
-                    image_primary: imagelink,
-                    price: price,
-                    availability: availability,
-                    url: url
-                };
-                let payloadStr = JSON.stringify(payload);
-                response.setHeader("Content-Type", "application/json");
-                response.setHeader("Access-Control-Allow-Origin","*");
-                response.writeHead(200);
-                response.write(payloadStr);
-                response.end();
-              });
-            }).on('error', (error) => {
-                response.writeHead(404);
-                response.write('Houve erro na requisição');
-                response.end();
-                console.error(error);
-              });
-                break;
-                case '/status':
-                    response.writeHead(200, {'Content-Type': 'text/html'});
-                    response.write('OK');
-                    response.end();
-                    break;
-                    default:
-                        response.writeHead(404);
-                        response.write('Rota não definida');
-                        response.end();
-        }
+  handleRequest: function (request, response) {
+    var https = require("https");
+    var cheerio = require("cheerio");
+    var dataURL = [];
+    var baseURL = "https://" + request.headers.host + "/";
+    let path = new URL(request.url, baseURL);
+    switch (path.pathname) {
+      case "/scrap_product":
+        https
+          .get(path.searchParams.get("url"), (res) => {
+            res.on("data", (data) => {
+              dataURL.push(data);
+            });
+            res.on("end", () => {
+              dataURL = Buffer.concat(dataURL).toString();
+              let $ = cheerio.load(dataURL);
+              var availability = true;
+
+              if ($(".unavailable__product").length) {
+                var title = $("h1").text();
+                var imagelink = $(".unavailable__product-img").attr("src");
+                var priceJSON = JSON.parse(
+                  $(".icon-fav-outline").attr("data-product")
+                );
+                var price = priceJSON.price;
+                var url = path.searchParams.get("url");
+                availability = false;
+              } else {
+                var title = $("h1").text();
+                var imagelink = $(".showcase-product__big-img").attr("src");
+                var price = parseFloat(
+                  $(".price-template__text").text().replace(",", ".")
+                );
+                var url = path.searchParams.get("url");
+              }
+
+              let productJson = {
+                name: title,
+                image_primary: imagelink,
+                price: price,
+                availability: availability,
+                url: url,
+              };
+              let product = JSON.stringify(productJson);
+              response.setHeader("Content-Type", "application/json");
+              response.setHeader("Access-Control-Allow-Origin", "*");
+              response.writeHead(200);
+              response.write(product);
+              response.end();
+            });
+          })
+          .on("error", (error) => {
+            response.writeHead(404);
+            response.write("Houve erro na requisição");
+            response.end();
+            console.error(error);
+          });
+        break;
+      case "/status":
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.write("OK");
+        response.end();
+        break;
+      default:
+        response.writeHead(404);
+        response.write("Rota não definida");
+        response.end();
     }
+  },
 };
